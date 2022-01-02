0f31285b014aa33a54cc3a22bcf005717decca84
Frederik <5511687+fightforlife@users.noreply.github.com>
2022-01-02 13:15:09 +0100

    add individual arguments

--- start_services.sh
+++ start_services.sh
@@ -3,7 +3,18 @@
 #detect sensors and load modules
 sensors-detect --auto | sed -n '/# Chip drivers/,/#----cut here----/{//!p;}' | xargs -n1 modprobe
 
-hddfancontrol $1 &
+hddfancontrol -d $DEVICES  \
+              -p $PWM_DEVICES \
+              --pwm-start-value $PWM_START \
+              --pwm-stop-value $PWM_STOP \
+              --min-temp $MIN_TEMP \
+              --max-temp $MAX_TEMP \
+              --min-fan-speed-prct $MIN_FAN \
+              -i $INTERVALL \
+              --spin-down-time $SPINDOWN_TIME \
+              --smartctl \
+              -l $LOG_PATH \
+              &
   
 # Wait for any process to exit
 wait -n
