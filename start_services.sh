7a23ee45458501f8b6da6680611a313cd5384275
Frederik <5511687+fightforlife@users.noreply.github.com>
2022-05-02 13:17:46 +0200

    reverse uno

--- start_services.sh
+++ start_services.sh
@@ -6,17 +6,17 @@ sensors-detect --auto | sed -n '/# Chip drivers/,/#----cut here----/{//!p;}' | x
 #create argument array
 declare -a args=()
 
-[[ -z $DEVICES ]] && args+=(--drives $DEVICES)
-[[ -z $PWM_DEVICES ]] && args+=(--pwm $PWM_DEVICES)
-[[ -z $PWM_START ]] && args+=(--pwm-start-value $PWM_START)
-[[ -z $PWM_STOP ]] && args+=(--pwm-stop-value $PWM_STOP)
-[[ -z $MIN_TEMP ]] && args+=(--min-temp $MIN_TEMP)
-[[ -z $MAX_TEMP ]] && args+=(--max-temp $MAX_TEMP)
-[[ -z $MIN_FAN ]] && args+=(--min-fan-speed-prct $MIN_FAN)
-[[ -z $INTERVALL ]] && args+=(-i $INTERVALL)
-[[ -z $TEMP_QUERY_MODE ]] && args+=(--$TEMP_QUERY_MODE)
-[[ -z $SPINDOWN_TIME ]] && args+=(--spin-down-time $SPINDOWN_TIME)
-[[ -z $LOG_PATH ]] && args+=(-l $LOG_PATH)
+[[ ! -z $DEVICES ]] && args+=(--drives $DEVICES)
+[[ ! -z $PWM_DEVICES ]] && args+=(--pwm $PWM_DEVICES)
+[[ ! -z $PWM_START ]] && args+=(--pwm-start-value $PWM_START)
+[[ ! -z $PWM_STOP ]] && args+=(--pwm-stop-value $PWM_STOP)
+[[ ! -z $MIN_TEMP ]] && args+=(--min-temp $MIN_TEMP)
+[[ ! -z $MAX_TEMP ]] && args+=(--max-temp $MAX_TEMP)
+[[ ! -z $MIN_FAN ]] && args+=(--min-fan-speed-prct $MIN_FAN)
+[[ ! -z $INTERVALL ]] && args+=(-i $INTERVALL)
+[[ ! -z $TEMP_QUERY_MODE ]] && args+=(--$TEMP_QUERY_MODE)
+[[ ! -z $SPINDOWN_TIME ]] && args+=(--spin-down-time $SPINDOWN_TIME)
+[[ ! -z $LOG_PATH ]] && args+=(-l $LOG_PATH)
 
 echo ${args[@]}
 
