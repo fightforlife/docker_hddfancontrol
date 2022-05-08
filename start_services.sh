229d03f7f2eb4c625d29ec6e59345b0995209bc1
Frederik <5511687+fightforlife@users.noreply.github.com>
2022-05-08 11:46:51 +0200

    optional params and temp query mode (#4)
    * first test
    
    * autobuild dev
    
    * fix
    
    * tests
    
    * further test
    
    * arg array
    
    * optional args and workflow
    
    * test
    
    * test2
    
    * test3
    
    * reverse uno
    
    * Update README.md

--- start_services.sh
+++ start_services.sh
@@ -3,19 +3,25 @@
 #detect sensors and load modules
 sensors-detect --auto | sed -n '/# Chip drivers/,/#----cut here----/{//!p;}' | xargs -n1 modprobe
 
-hddfancontrol -d $DEVICES  \
-              -p $PWM_DEVICES \
-              --pwm-start-value $PWM_START \
-              --pwm-stop-value $PWM_STOP \
-              --min-temp $MIN_TEMP \
-              --max-temp $MAX_TEMP \
-              --min-fan-speed-prct $MIN_FAN \
-              -i $INTERVALL \
-              --spin-down-time $SPINDOWN_TIME \
-              --smartctl \
-              -l $LOG_PATH \
-              &
-  
+#create argument array
+declare -a args=()
+
+[[ ! -z $DEVICES ]] && args+=(--drives $DEVICES)
+[[ ! -z $PWM_DEVICES ]] && args+=(--pwm $PWM_DEVICES)
+[[ ! -z $PWM_START ]] && args+=(--pwm-start-value $PWM_START)
+[[ ! -z $PWM_STOP ]] && args+=(--pwm-stop-value $PWM_STOP)
+[[ ! -z $MIN_TEMP ]] && args+=(--min-temp $MIN_TEMP)
+[[ ! -z $MAX_TEMP ]] && args+=(--max-temp $MAX_TEMP)
+[[ ! -z $MIN_FAN ]] && args+=(--min-fan-speed-prct $MIN_FAN)
+[[ ! -z $INTERVALL ]] && args+=(-i $INTERVALL)
+[[ ! -z $TEMP_QUERY_MODE ]] && args+=(--$TEMP_QUERY_MODE)
+[[ ! -z $SPINDOWN_TIME ]] && args+=(--spin-down-time $SPINDOWN_TIME)
+[[ ! -z $LOG_PATH ]] && args+=(-l $LOG_PATH)
+
+echo ${args[@]}
+
+hddfancontrol ${args[@]} &
+
 # Wait for any process to exit
 wait -n
   
