0dbde50047a949600564756080aaad7b7d1a7280
fightforlife <5511687+fightforlife@users.noreply.github.com>
2025-08-17 22:25:19 +0200

    handle initialisatio of sensors better

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ FROM alpine:latest
 RUN apk add --no-cache \
     -X http://dl-cdn.alpinelinux.org/alpine/edge/testing smartmontools hdparm sdparm lm-sensors \
     -X http://dl-cdn.alpinelinux.org/alpine/edge/community hd-idle \
-    -X http://dl-cdn.alpinelinux.org/alpine/edge/main kmod lm-sensors lm-sensors-detect lm-sensors-fancontrol coreutils
+    -X http://dl-cdn.alpinelinux.org/alpine/edge/main sed nvme-cli kmod lm-sensors lm-sensors-detect lm-sensors-fancontrol coreutils
 
 # Copy the built binary from the builder stage
 COPY --from=builder /app/hddfancontrol/target/release/hddfancontrol /usr/local/bin/hddfancontrol
--- start_services.sh
+++ start_services.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-#set -euo pipefail
+set -euo pipefail
 
 # Define a variable to hold the list of modules
-MODULES=$(sensors-detect --auto | sed -n '/# Chip drivers/,/#----cut here----/{//!p;}')
+COMMANDS=$(sensors-detect --auto | sed -n '/# Chip drivers/,/#----cut here----/{//!p;}')
 
-# Check if any modules were detected
-if [ -z "$MODULES" ]; then
-    echo "No sensor modules detected. Exiting."
+# Check if the COMMANDS variable is not empty before trying to execute it.
+if [ -z "$COMMANDS" ]; then
+    echo "No commands were extracted from 'sensors-detect --auto'."
     exit 1
+else
+    echo "The following commands will be run:"
+    echo "-----------------------------------"
+    echo "$COMMANDS"
+    echo "-----------------------------------"
+    # Read each line from the COMMANDS variable and execute it
+    echo "$COMMANDS" | while read -r cmd; do
+        if [ -n "$cmd" ]; then
+            stdbuf -oL sh -c "$cmd" 2>&1 | sed 's/^/[init] /' &
+        fi
+    done
+
+
+    # Wait for all background jobs to complete
+    wait
+
+    # Print a success message after all commands have run
+    echo "[init] All sensors-detect commands were run successfully."
 fi
-
-echo "Detected modules: $MODULES"
-
-# Loop through each module and load it, providing feedback
-for module in $MODULES; do
-    echo "Attempting to load module: $module"
-    if modprobe "$module"; then
-        echo "Successfully loaded $module"
-    else
-        echo "Error: Failed to load $module"
-    fi
-done
-
 
 # Create argument array
 declare -a hddfancontrol_args=()
 [[ -n ${DRIVES:-} ]] && hddfancontrol_args+=(--drives "$DRIVES")
 [[ -n ${PWM:-} ]] && hddfancontrol_args+=(--pwm "$PWM")
-[[ -n ${DRIVE_TEMP_RANGE:-} ]] && hddfancontrol_args+=(--drive_temp_range "$DRIVE_TEMP_RANGE")
-[[ -n ${MIN_FAN_SPEED_PRCT:-} ]] && hddfancontrol_args+=(--min_fan_speed_prct "$MIN_FAN_SPEED_PRCT")
+[[ -n ${DRIVE_TEMP_RANGE:-} ]] && hddfancontrol_args+=(--drive-temp-range ${DRIVE_TEMP_RANGE})
+[[ -n ${MIN_FAN_SPEED_PRCT:-} ]] && hddfancontrol_args+=(--min-fan-speed-prct "$MIN_FAN_SPEED_PRCT")
 [[ -n ${INTERVAL:-} ]] && hddfancontrol_args+=(--interval "$INTERVAL")
 [[ -n ${HWMONS:-} ]] && hddfancontrol_args+=(--hwmons "$HWMONS")
-[[ -n ${RESTORE_FAN_SETTINGS:-} ]] && hddfancontrol_args+=(--restore_fan_settings "$RESTORE_FAN_SETTINGS")
-
+[[ -n ${RESTORE_FAN_SETTINGS:-} ]] && hddfancontrol_args+=(--restore-fan-settings)
 
 stdbuf -oL hddfancontrol -v "${VERBOSITY:-INFO}" daemon "${hddfancontrol_args[@]}" 2>&1 | sed 's/^/[hddfancontrol] /' &
-
 
 # Create argument array
 declare -a hdidle_args=()
